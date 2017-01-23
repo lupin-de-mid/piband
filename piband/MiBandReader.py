048fadab5ee79e1f1e6e007c58e8f0e91023b529
Demid Lupin <lupin@demid.su>
2017-01-23 03:32:46 +0300

    Added docs

--- piband/MiBandReader.py
+++ piband/MiBandReader.py
@@ -10,6 +10,13 @@ BatteryUUID = "0000ff0c-0000-1000-8000-00805f9b34fb"
 
 
 def read_MiBand(mac):
+    """
+    Read mi band state bu mac
+    :type mac: str
+    :rtype: MiBandState
+    :param mac: bluetouth mac of miband
+    :return: state of mi band
+    """
     requester = GATTRequester(mac, False)
     requester.connect(True)
     data = requester.read_by_uuid(
