807d6be926a62a2830d18cd09654e841c6eae5ba
Demid Lupin <lupin@demid.su>
2017-01-23 03:18:29 +0300

    Reformat file

--- piband/MiBandReader.py
+++ piband/MiBandReader.py
@@ -8,10 +8,11 @@ from piband.MiBandState import MiBandState
 
 BatteryUUID = "0000ff0c-0000-1000-8000-00805f9b34fb"
 
+
 def read_MiBand(mac):
     requester = GATTRequester(mac, False)
     requester.connect(True)
     data = requester.read_by_uuid(
         BatteryUUID)
     print("Device name: ")
-    return MiBandState(mac,data[0].encode())
+    return MiBandState(mac, data[0].encode())
